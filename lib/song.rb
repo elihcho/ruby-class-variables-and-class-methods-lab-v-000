22e5cf2fd115e46c0d1db432afc929e6b9662ad8
Eli Cho <eli.cho@gmail.com>
2018-08-06 07:22:48 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << genre
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres.uniq
+  end
+  
+  def artists
+    @@artists
+  end
+  
+end
